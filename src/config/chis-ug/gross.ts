12cc0c2cfc73d9587b3499845cd809bf048a58a3
kennsippell@gmail.com <kennsippell@gmail.com>
2024-01-25 17:24:19 -0600

    Passing lint

--- src/config/chis-ug/gross.ts
+++ src/config/chis-ug/gross.ts
@@ -1,7 +1,7 @@
 import { PlacePayload } from '../../lib/cht-api';
 
 export default async function mutate(payload: PlacePayload): Promise<PlacePayload> {
-  if (payload.contact_type == 'health_center') {
+  if (payload.contact_type === 'health_center') {
     // during replacement, the name is optional
     if (payload.name) {
       payload.name += ' Area (village)';
