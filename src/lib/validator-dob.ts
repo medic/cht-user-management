f9032f9a860c2911bc7ac9bc33beb642b26a5cd5
kennsippell <kennsippell@gmail.com>
2024-04-02 21:17:49 +0700

    Code Review feedback

--- src/lib/validator-dob.ts
+++ src/lib/validator-dob.ts
@@ -1,10 +1,10 @@
 import { DateTime } from 'luxon';
 import { IValidator } from './validation';
 
-class ValidatorDateOfBirth implements IValidator {
+export default class ValidatorDateOfBirth implements IValidator {
   isValid(input: string) : boolean {
     try {
-      const parsed = parse(input);
+      const parsed = this.parse(input);
       return parsed.isValid && parsed < DateTime.now();
     } catch (e) {
       return false;
@@ -12,7 +12,7 @@ class ValidatorDateOfBirth implements IValidator {
   }
 
   format(input : string) : string {
-    const parsed = parse(input);
+    const parsed = this.parse(input);
     const asISODate = parsed.toISODate();
     if (!this.isValid(input) || !asISODate) {
       return input;
@@ -22,43 +22,16 @@ class ValidatorDateOfBirth implements IValidator {
   }
 
   get defaultError(): string {
-    return 'Not a valid Date of Birth (eg. 1990-02-26)';
-  }
-}
-
-const parse = (input: string) => {
-  const strippedInput = input.replace(/ /ig, '');
-  return DateTime.fromISO(strippedInput);
-};
-
-export default class ValidatorAge implements IValidator {
-  private dobValidator = new ValidatorDateOfBirth();
-
-  private isAgeValid(age: number): boolean {
-    return !isNaN(age) && age > 0;
+    return 'Not a valid Date of Birth (eg. 1990-02-26 or 25)';
   }
 
-  private trimSpace(input: string): string {
-    return input.replace(/\s/g, '');
-  }
-
-  isValid(input: string): boolean {
-    const age = Number(this.trimSpace(input));
-    return this.isAgeValid(age) || this.dobValidator.isValid(input);
-  }
-
-  format(input: string): string {
-    const age = Number(this.trimSpace(input));
-    if (this.isAgeValid(age)) {
-      return DateTime.now().minus({years: age}).toISODate();
+  private parse(input: string): DateTime {
+    const strippedInput = input.replace(/\s/ig, '');
+    const asNumber = Number(strippedInput);
+    if (!isNaN(asNumber) && asNumber > 0) {
+      return DateTime.now().minus({ years: asNumber });
     }
-    if (this.dobValidator.isValid(input)) {
-      return this.dobValidator.format(input);
-    }
-    return input;
-  }
 
-  get defaultError(): string {
-    return 'Age should be a number between 18 and 200 or a valid date (eg. 1990-02-26)';
-  }
+    return DateTime.fromISO(strippedInput);
+  };
 }
