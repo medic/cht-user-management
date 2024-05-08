c8bb952de5a1a619f638034ab53a1c82b7af482f
freddieptf <muiru@medic.org>
2024-05-09 00:10:42 +0300

    support row updates

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -27,6 +27,27 @@ export default async function events(fastify: FastifyInstance) {
     });
   });
 
+  fastify.get('/events/place/:id', async (req, resp) => {
+    const { id } = req.params as any;
+    const sessionCache: SessionCache = req.sessionCache;
+    const directiveModel = new DirectiveModel(sessionCache, req.cookies.filter);
+    const place = sessionCache.getPlace(id);
+    if (!place) {
+      return resp.view('src/liquid/place/directive.html', {
+        directiveModel
+      });
+    }
+    return resp.view('src/liquid/place/place_event.html', {
+      contactType: {
+        ...place.type,
+        hierarchy: Config.getHierarchyWithReplacement(place.type, 'desc'),
+        userRoleProperty: Config.getUserRoleConfig(place.type),
+      },
+      place: place,
+      directiveModel
+    });
+  });
+
   fastify.get('/events/connection', async (req, resp) => {
     const uploadManager: UploadManager = fastify.uploadManager;
 
@@ -34,13 +55,15 @@ export default async function events(fastify: FastifyInstance) {
     const placesChangeListener = (arg: string = '*') => {
       resp.sse({ event: 'place_state_change', data: arg });
     };
-    
+    const placeChangeListener = (arg: string) => {
+      resp.sse({ event: `update-${arg}`, data: arg});
+    };    
     uploadManager.on('refresh_table', placesChangeListener);
-    uploadManager.on('refresh_table_row', placesChangeListener);
+    uploadManager.on('refresh_table_row', placeChangeListener);
 
     req.socket.on('close', () => {
       uploadManager.removeListener('refresh_table', placesChangeListener);
-      uploadManager.removeListener('refresh_table_row', placesChangeListener);
+      uploadManager.removeListener('refresh_table_row', placeChangeListener);
     });
   });
 }
