a18c99a37a9c856847d7e6289c4b59e45321cbef
kennsippell <kennsippell@gmail.com>
2024-05-21 17:45:13 -0700

    Fix eslint

--- scripts/move-contacts/index.ts
+++ scripts/move-contacts/index.ts
@@ -1,21 +1,21 @@
-import { queueManager } from "../../shared/queues";
-import { Config } from "../../src/config";
-import { ChtApi } from "../../src/lib/cht-api";
-import ChtSession from "../../src/lib/cht-session";
-import MoveLib from "../../src/lib/move";
-import SessionCache from "../../src/services/session-cache";
+import { queueManager } from '../../shared/queues';
+import { Config } from '../../src/config';
+import { ChtApi } from '../../src/lib/cht-api';
+import ChtSession from '../../src/lib/cht-session';
+import MoveLib from '../../src/lib/move';
+import SessionCache from '../../src/services/session-cache';
 
-import allPlacesToMove from "./laikipia-west";
+import allPlacesToMove from './laikipia-west.json';
 
 const username = 'kenn_sippell_medic_user_manager';
 const password = '';
 const authInfo = Config.getAuthenticationInfo('laikipia.echis.go.ke');
 const contactType = Config.getContactType('d_community_health_volunteer_area');
-const batchToMove = allPlacesToMove.slice(7, 50);
+const batchToMove = allPlacesToMove.slice(51, 52);
 
 (async () => {
   for (const toMove of batchToMove) {
-    const [from_SUBCOUNTY, from_CHU, from_replacement, to_SUBCOUNTY, to_CHU] = toMove;
+    const [from_SUBCOUNTY, from_CHU, from_replacement, to_SUBCOUNTY, to_CHU] = toMove as any[];
     const formData = {
       from_SUBCOUNTY, from_CHU, from_replacement,
       to_SUBCOUNTY, to_CHU,
