8fbe4e3e7a2fa64caef825b859ff6c94554c47b7
kennsippell <kennsippell@gmail.com>
2024-05-29 13:33:17 -0700

    More types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import { Config } from './config';
+import { Config, ContactType } from './config';
 import { ChtApi } from './lib/cht-api';
 import ChtSession from './lib/cht-session';
 import RemotePlaceCache from './lib/remote-place-cache';
-import SessionCache from './services/session-cache';
+import RemotePlaceResolver from './lib/remote-place-resolver';
+import Place from './services/place';
+import PlaceFactory from './services/place-factory';
+import SessionCache from './services/session-cache'
 
 export {
   Config,
+  ContactType,
   ChtApi,
   ChtSession,
-  SessionCache,
+  Place,
+  PlaceFactory,
   RemotePlaceCache,
+  SessionCache,
+  RemotePlaceResolver,
 };
+
+
