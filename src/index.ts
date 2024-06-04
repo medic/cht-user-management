349fe2282b62c60a63d4ace1542b170ef3f6c996
kennsippell <kennsippell@gmail.com>
2024-06-03 17:26:40 -0700

    More exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Config, ContactType } from './config';
 import { ChtApi } from './lib/cht-api';
 import ChtSession from './lib/cht-session';
+import MoveLib from './lib/move';
 import RemotePlaceCache from './lib/remote-place-cache';
 import RemotePlaceResolver from './lib/remote-place-resolver';
 import Place from './services/place';
 import PlaceFactory from './services/place-factory';
 import SessionCache from './services/session-cache'
+import { queueManager } from './shared/queues';
 
 export {
   Config,
@@ -17,6 +19,8 @@ export {
   RemotePlaceCache,
   SessionCache,
   RemotePlaceResolver,
+  MoveLib,
+  queueManager,
 };
 
 
