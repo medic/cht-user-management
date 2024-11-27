52a1453ad5267b599f5ec50537afdd2b880e4c2c
paulpascal <paulalogno@medic.org>
2024-11-27 10:31:32 +0000

    fix: increase session token exp to 96h

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import ChtSession from './cht-session';
 
 const LOGIN_EXPIRES_AFTER_MS = 2 * 24 * 60 * 60 * 1000;
-const QUEUE_SESSION_EXPIRATION = '48h';
+const QUEUE_SESSION_EXPIRATION = '96h';
 const { COOKIE_PRIVATE_KEY, WORKER_PRIVATE_KEY } = process.env;
 const PRIVATE_KEY_SALT = '_'; // change to logout all users
 const COOKIE_SIGNING_KEY = COOKIE_PRIVATE_KEY + PRIVATE_KEY_SALT;
