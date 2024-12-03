527ef592c94f6dcdbc7a776c21bb40a749187a08
kennsippell <kennsippell@gmail.com>
2024-12-03 02:32:56 -0800

    Avoid crash when cache is empty

--- src/lib/remote-place-cache.ts
+++ src/lib/remote-place-cache.ts
@@ -51,7 +51,7 @@ export default class RemotePlaceCache {
       RemotePlaceCache.cache = {};
     } else if (!contactTypeName) {
       delete RemotePlaceCache.cache[domain];
-    } else {
+    } else if (RemotePlaceCache.cache[domain]) {
       delete RemotePlaceCache.cache[domain][contactTypeName];
     }
   }
