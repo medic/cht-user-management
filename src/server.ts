936c6ce50d55f144539114a7fc0dbee67ad3e6ec
kennsippell <kennsippell@gmail.com>
2024-12-03 00:19:59 -0800

    Move server entrypoint to /server.js

--- src/server.ts
+++ src/server.ts
@@ -1,85 +1,32 @@
-import Fastify, { FastifyInstance, FastifyReply, FastifyRequest, FastifyServerOptions } from 'fastify';
-import autoload from '@fastify/autoload';
-import cookie from '@fastify/cookie';
-import formbody from '@fastify/formbody';
-import multipart from '@fastify/multipart';
-import fastifyStatic from '@fastify/static';
-import fastifyCompress from '@fastify/compress';
-import view from '@fastify/view';
-import { Liquid } from 'liquidjs';
-import { FastifySSEPlugin } from 'fastify-sse-v2';
-import path from 'path';
-const metricsPlugin = require('fastify-metrics');
+require('dotenv').config();
+import build from './build';
+import { env } from 'process';
+const {
+  INTERFACE
+} = process.env;
 
-import Auth from './lib/authentication';
-import SessionCache from './services/session-cache';
-import { checkRedisConnection } from './config/config-worker';
+const port: number = env.PORT ? parseInt(env.PORT) : 3500;
 
-const build = (opts: FastifyServerOptions): FastifyInstance => {
-  const fastify = Fastify(opts);
-  fastify.register(formbody);
-  fastify.register(multipart);
-  fastify.register(FastifySSEPlugin);
-  fastify.register(cookie);
-  fastify.register(fastifyCompress);
-  fastify.register(view, {
-    engine: {
-      liquid: new Liquid({ 
-        extname: '.html', 
-        root: 'src/liquid', 
-        cache: process.env.NODE_ENV === 'production', 
-        jekyllInclude: true, 
-        dynamicPartials: true 
-      }),
+(async () => {
+  const loggerConfig = {
+    transport: {
+      target: 'pino-pretty',
     },
+  };
+  const server = build({
+    logger: loggerConfig,
   });
-  fastify.register(autoload, {
-    dir: path.join(__dirname, 'plugins'),
-  });
-  fastify.register(autoload, {
-    dir: path.join(__dirname, 'routes'),
-  });
-  fastify.register(fastifyStatic, {
-    root: path.join(__dirname, '../src/public'),
-    prefix: '/public/',
-    serve: true,
-  });
-  
-  fastify.register(metricsPlugin, {
-    endpoint: '/metrics',
-    routeMetrics: {
-      enabled: {
-        histogram: true,
-        summary: false
-      }
+
+  // in 1.1.0 we allowed INTERFACE to be declared in .env, but let's be
+  // backwards compatible to when it was undeclared and hard coded to
+  // be 0.0.0.0
+  let calculated_interface = '0.0.0.0';
+  if (INTERFACE) {
+    calculated_interface = INTERFACE;
+  }
+  server.listen({ host: calculated_interface, port }, (err) => {
+    if (err) {
+      throw err;
     }
   });
-
-  Auth.assertEnvironmentSetup();
-  checkRedisConnection();
-
-  fastify.addHook('preValidation', async (req: FastifyRequest, reply: FastifyReply) => {
-    if (req.unauthenticated || req.routeOptions.url === '/public/*' || req.routeOptions.url === '/metrics') {
-      return;
-    }
-
-    const cookieToken = req.cookies[Auth.AUTH_COOKIE_NAME] as string;
-    if (!cookieToken) {
-      reply.redirect('/login');
-      throw new Error('user must login');
-    }
-
-    try {
-      const chtSession = Auth.decodeTokenForCookie(cookieToken);
-      req.chtSession = chtSession;
-      req.sessionCache = SessionCache.getForSession(chtSession);
-    } catch (e) {
-      reply.redirect('/login');
-      throw e;
-    }
-  });
-
-  return fastify;
-};
-
-export default build;
+})();
