97aea6ed033d87165243884866dbd57f8653cb62
Kenn Sippell <kennsippell@gmail.com>
2025-01-09 15:15:22 -0700

    Warning System (#167)

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 import { FastifyInstance } from 'fastify';
 import JSZip from 'jszip';
 import { stringify } from 'csv/sync';
 
-import { Config } from '../config';
+import { Config, ContactProperty } from '../config';
 import getCredentialsFiles from '../lib/credentials-file';
 import SessionCache from '../services/session-cache';
 
@@ -10,7 +12,7 @@ export default async function files(fastify: FastifyInstance) {
   fastify.get('/files/template/:placeType', async (req) => {
     const params: any = req.params;
     const placeType = params.placeType;
-    const columns = Config.getCsvTemplateColumns(placeType);
+    const columns = getCsvTemplateColumns(placeType);
     return stringify([columns]);
   });
 
@@ -26,4 +28,22 @@ export default async function files(fastify: FastifyInstance) {
     reply.header('Content-Disposition', `attachment; filename="${Date.now()}_${req.chtSession.authInfo.friendly}_users.zip"`);
     return zip.generateNodeStream();
   });
+}
+
+function getCsvTemplateColumns(placeType: string) {
+  const placeTypeConfig = Config.getContactType(placeType);
+  const hierarchy = Config.getHierarchyWithReplacement(placeTypeConfig);
+  const userRoleConfig = Config.getUserRoleConfig(placeTypeConfig);
+
+  const extractColumns = (properties: ContactProperty[]) => properties
+    .filter(p => p.type !== 'generated')
+    .map(p => p.friendly_name);
+
+  const columns = _.uniq([
+    ...hierarchy.map(p => p.friendly_name),
+    ...extractColumns(placeTypeConfig.place_properties),
+    ...extractColumns(placeTypeConfig.contact_properties),
+    ...(Config.hasMultipleRoles(placeTypeConfig) ? [userRoleConfig.friendly_name] : []),
+  ]);
+  return columns;
 }
