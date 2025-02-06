44841e54c02e31700fd25b45f1f6c65a3c252f5b
Ben Kiarie <kiarie@medic.org>
2025-02-06 17:12:20 +0300

    feat: prevent login without required permissions

--- src/lib/cht-session.ts
+++ src/lib/cht-session.ts
@@ -12,6 +12,7 @@ import { RemotePlace } from './remote-place-cache';
 const COUCH_AUTH_COOKIE_NAME = 'AuthSession=';
 const ADMIN_FACILITY_ID = '*';
 const ADMIN_ROLES = ['admin', '_admin'];
+const AUTHORIZED_ROLES = [...ADMIN_ROLES, 'user_manager'];
 
 axiosRetry(axios, axiosRetryConfig);
 
@@ -105,6 +106,14 @@ export default class ChtSession {
       throw e;
     }
   }
+
+  private static validateUserRole(userDoc: any, username: string): void {
+    const hasRequiredRole = (roles: string[]): boolean => _.intersection(AUTHORIZED_ROLES, roles).length > 0;
+
+    if (!hasRequiredRole(userDoc?.roles || [])) {
+      throw Error(`User ${username} role does not have the required permissions`);
+    }
+  }
   
   private static async fetchCreationDetails(authInfo: AuthenticationInfo, username: string, sessionToken: string): Promise<SessionCreationDetails> {
     // api/v2/users returns all users prior to 4.6 even with ?facility_id
@@ -122,6 +131,8 @@ export default class ChtSession {
     ] = await Promise.all(fetches);
 
     const isAdmin = _.intersection(ADMIN_ROLES, userDoc?.roles).length > 0;
+
+    ChtSession.validateUserRole(userDoc, username);
 
     const facilityIds = isAdmin ? [ADMIN_FACILITY_ID] : _.flatten([userDoc?.facility_id]).filter(Boolean);
     if (!facilityIds?.length) {
