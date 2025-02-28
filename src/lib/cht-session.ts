d7d0623f59b6a1f2b3542fba2327987acf9dafe6
Ben Kiarie <2597305+Benmuiruri@users.noreply.github.com>
2025-02-28 14:41:49 +0300

    feat(#243): prevent login into UMT without required permissions (#250)
    * feat: prevent login without required permissions
    
    * chore: better naming
    
    * chore: check permission instead of role
    
    * chore: move permissions to service
    
    * Clean up
    
    * chore: add helpful log
    
    * 2.1.4
    
    * chore: bump version
    
    * chore: feedback
    
    * chore: update test

--- src/lib/cht-session.ts
+++ src/lib/cht-session.ts
@@ -8,10 +8,11 @@ import { AuthError } from './authentication-error';
 import { AuthenticationInfo } from '../config';
 import { axiosRetryConfig } from './retry-logic';
 import { RemotePlace } from './remote-place-cache';
+import { UserPermissionService } from '../services/user-permissions';
 
 const COUCH_AUTH_COOKIE_NAME = 'AuthSession=';
 const ADMIN_FACILITY_ID = '*';
-const ADMIN_ROLES = ['admin', '_admin'];
+export const ADMIN_ROLES = ['admin', '_admin'];
 
 axiosRetry(axios, axiosRetryConfig);
 
@@ -108,7 +109,7 @@ export default class ChtSession {
   
   private static async fetchCreationDetails(authInfo: AuthenticationInfo, username: string, sessionToken: string): Promise<SessionCreationDetails> {
     // api/v2/users returns all users prior to 4.6 even with ?facility_id
-    const paths = [`medic/org.couchdb.user:${username}`, 'api/v2/monitoring'];
+    const paths = [`medic/org.couchdb.user:${username}`, 'api/v2/monitoring', 'api/v1/settings'];
     const fetches = paths.map(path => {
       const url = ChtSession.createUrl(authInfo, path);
       return axios.get(
@@ -118,10 +119,13 @@ export default class ChtSession {
     });
     const [
       { data: userDoc }, 
-      { data: { version: { app: chtCoreVersion } } }
+      { data: { version: { app: chtCoreVersion } } },
+      { data: settings }
     ] = await Promise.all(fetches);
 
     const isAdmin = _.intersection(ADMIN_ROLES, userDoc?.roles).length > 0;
+
+    UserPermissionService.validateUserPermissions(userDoc, username, settings.permissions);
 
     const facilityIds = isAdmin ? [ADMIN_FACILITY_ID] : _.flatten([userDoc?.facility_id]).filter(Boolean);
     if (!facilityIds?.length) {
