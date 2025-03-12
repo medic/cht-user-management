afe40dcad8707d79caaa64cee28cc67aafc2534d
jonathanbataire <jonathanbataire@gmail.com>
2025-03-12 16:29:11 +0300

    revert tests

--- test/config.spec.ts
+++ test/config.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
 import { Config, PartnerConfig } from '../src/config';
-import { DEFAULT_CONFIG_MAP } from '../src/config/config-factory';
+import DEFAULT_CONFIG_MAP from '../src/config/config-factory';
 import { mockSimpleContactType } from './mocks';
 
 const mockPartnerConfig = (): PartnerConfig => ({
@@ -18,22 +18,25 @@ describe('config', () => {
     Config.assertValid(mockConfig);
   });
 
-  it('assert on unknown property type', async () => {
+  it('assert on unknown property type', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].hierarchy[0].type = 'unknown';
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('type "unknown"');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('type "unknown"');
   });
   
-  it('place name is always required', async () => {
+  it('place name is always required', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].place_properties.shift();
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('"name"');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('"name"');
   });
 
-  it('contact name is always required', async () => {
+  it('contact name is always required', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].contact_properties.shift();
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('"name"');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('"name"');
   });
 
   it('contact_properties can have unique attributes', () => {
@@ -42,34 +45,38 @@ describe('config', () => {
     Config.assertValid(mockConfig);
   });
 
-  it('hierarchy properties cannot have unique attributes', async () => {
+  it('hierarchy properties cannot have unique attributes', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].hierarchy[0].unique = 'parent';
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('with "unique" values');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('with "unique" values');
   });
 
-  it('parent hierarchy level is required', async () => {
+  it('parent hierarchy level is required', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].hierarchy[0].level = 2;
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('with parent level');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('with parent level');
   });
   
-  it('#124 - cannot have generated property in hierarchy', async () => {
+  it('#124 - cannot have generated property in hierarchy', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].hierarchy[0].type = 'generated';
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('cannot be of type "generated"');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('cannot be of type "generated"');
   });
 
-  it('#124 - cannot have generated property as replacement_property', async () => {
+  it('#124 - cannot have generated property as replacement_property', () => {
     const mockConfig = mockPartnerConfig();
     mockConfig.config.contact_types[0].replacement_property.type = 'generated';
-    await expect(Config.assertValid(mockConfig)).to.be.rejectedWith('cannot be of type "generated"');
+    const assertion = () => Config.assertValid(mockConfig);
+    expect(assertion).to.throw('cannot be of type "generated"');
   });
 
   const configs = Object.entries(DEFAULT_CONFIG_MAP);
   for (const [configName, partnerConfig] of configs) {
-    it(`config ${configName} is valid`, async () => {
-      await Config.assertValid(partnerConfig);
+    it(`config ${configName} is valid`, () => {
+      Config.assertValid(partnerConfig);
     });
   }
 });
