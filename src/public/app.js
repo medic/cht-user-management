39759850a6d00c9ba6791dda76670ab3064478f2
freddieptf <muiru@medic.org>
2025-05-26 09:59:28 +0300

    indent

--- src/public/app.js
+++ src/public/app.js
@@ -1,11 +1,12 @@
 (function() {
-	let failureCount = 0;
-	const maxFailures = 5;
-	document.addEventListener("htmx:sseError", function(evt) {
-		failureCount++;
-		if (failureCount >= maxFailures) {
-			console.log("Max failures reached. Stopping SSE connection.");
-			evt.target.removeAttribute("sse-connect");
-		}
-	});
+  let failureCount = 0;
+  const MAX_FAILURES = 5;
+  document.addEventListener("htmx:sseError", function(evt) {
+    failureCount++;
+    if (failureCount >= MAX_FAILURES) {
+      console.log("Max failures reached. Stopping SSE connection.");
+      evt.target.removeAttribute("sse-connect");
+      fetch("/").then((resp) => resp.redirected && window.location.replace(resp.url));
+    }
+  });
 })();
