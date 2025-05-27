40dac3878b6cc2e1e0a5084535ff4e2999cfaf1a
freddieptf <muiru@medic.org>
2025-05-27 14:44:11 +0300

    copy watchdog config

---
+++ release.config.js
@@ -0,0 +1,27 @@
+export default {
+  'branches': ['main'],
+  'plugins': [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      },
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version}',
+      },
+    ],
+  ],
+};
